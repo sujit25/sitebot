a1989da8a3ed4acb5324e944ad10068d110c0f47
Bhavani <bhavani.r@osdc.ffi.co.in>
2017-01-05 12:24:58 +0530

    update readme file

--- webhook.py
+++ webhook.py
@@ -30,11 +30,11 @@ def processRequest(req):
 def hello_world():
     req = request.get_json(silent=True, force=True)
 
-    #print("Request:")
-    #print(json.dumps(req, indent=4))
+    print("Request:")
+    print(json.dumps(req, indent=4))
     res = processRequest(req)
     res = json.dumps(res, indent=4)
     r = make_response(res)
-    #print res
+    print res
     r.headers['Content-Type'] = 'application/json'
     return r
