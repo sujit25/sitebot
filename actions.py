44fa762b7e93059fb073a0224711cac706d318be
bhavani.r <bhavani.r@focusite.com>
2017-01-05 10:09:09 +0530

    add action file

--- actions.py
+++ actions.py
@@ -6,7 +6,8 @@ def format_results(response_message,results):
         response_message += str(i)+") " +result+"\n"
         i += 1
     return response_message
-
+    
+#get response_message for products or services
 def get_info(entity):
     results = []
     response_message = "The following are the list of "+entity+": \n"
